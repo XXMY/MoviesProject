3f09e0a328c88cbd77e9eee23168cce47566af32
caifw <caifw@wasu.com>
2017-03-10 21:17:39 +0800

    Add init project modules

---
+++ movies-common/src/main/java/com/cfw/movies/commons/vo/HttpResponse.java
@@ -0,0 +1,55 @@
+package com.cfw.movies.commons.vo;
+
+/**
+ * @author Fangwei_Cai
+ * @time since 2016年4月11日 上午10:32:08
+ */
+public class HttpResponse {
+
+	private int code;
+
+	private String message;
+
+	private Object object;
+
+	public HttpResponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public HttpResponse(int code, String message) {
+		super();
+		this.code = code;
+		this.message = message;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Object getObject() {
+		return object;
+	}
+
+	public void setObject(Object object) {
+		this.object = object;
+	}
+
+	@Override
+	public String toString() {
+		return "AjaxRequestResult [code=" + code + ", message=" + message + ", object=" + object + "]";
+	}
+	
+}
