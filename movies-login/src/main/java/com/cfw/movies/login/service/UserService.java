89bf4750d09f5dff3eee3f0318617ba791aaaf0f
caifw <caifw@wasu.com>
2017-03-11 19:37:45 +0800

    reinit the project modules

---
+++ movies-login/src/main/java/com/cfw/movies/login/service/UserService.java
@@ -0,0 +1,32 @@
+package com.cfw.movies.login.service;
+
+
+/**
+ * 用户注册服务接口
+ * @author Fangwei_Cai
+ * @time since 2016年3月26日 下午4:38:30
+ */
+public interface UserService {
+	
+	/**
+	 * Check whether user exists.
+	 * @author Fangwei_Cai
+	 * @time since 2016年3月26日 下午7:38:17
+	 */
+	//boolean userExists(Users user);
+	
+	/**
+	 * The value of username attribute cannot empty.
+	 * @author Fangwei_Cai
+	 * @time since 2016年3月26日 下午8:05:26
+	 */
+	//boolean modifyUsersInfo(Users newUser);
+	
+	
+	/**
+	 * @author Fangwei_Cai
+	 * @time since 2016年3月27日 上午10:05:11
+	 */
+	//boolean register(Users user);
+
+}
---
+++ movies-manage/src/main/java/com/cfw/movies/manage/Application.java
@@ -0,0 +1,15 @@
+package com.cfw.movies.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Cfw on 2017/3/10.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String [] args){
+        SpringApplication.run(Application.class);
+    }
+}
