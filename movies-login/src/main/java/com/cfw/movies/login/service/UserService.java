d0d99d4bc17ad11e8114705b56dd248217220d7d
caifw <caifw@wasu.com>
2017-03-11 19:39:21 +0800

    reinit the project modules

--- movies-login/src/main/java/com/cfw/movies/login/service/UserService.java
+++ movies-login/src/main/java/com/cfw/movies/login/service/UserService.java
@@ -1,5 +1,6 @@
 package com.cfw.movies.login.service;
 
+import com.cfw.movies.commons.model.Users;
 
 /**
  * 用户注册服务接口
@@ -13,20 +14,20 @@ public interface UserService {
 	 * @author Fangwei_Cai
 	 * @time since 2016年3月26日 下午7:38:17
 	 */
-	//boolean userExists(Users user);
+	boolean userExists(Users user);
 	
 	/**
 	 * The value of username attribute cannot empty.
 	 * @author Fangwei_Cai
 	 * @time since 2016年3月26日 下午8:05:26
 	 */
-	//boolean modifyUsersInfo(Users newUser);
+	boolean modifyUsersInfo(Users newUser);
 	
 	
 	/**
 	 * @author Fangwei_Cai
 	 * @time since 2016年3月27日 上午10:05:11
 	 */
-	//boolean register(Users user);
+	boolean register(Users user);
 
 }
